77dee68e1796c534e5058569b4b4981a1a9ca6ef
nabettu <tatsuaki.w@gmail.com>
2015-11-21 16:25:17 +0900

    a

--- script.js
+++ script.js
@@ -2,40 +2,42 @@ var map;
 var panorama;
 
 function initialize() {
-  if (GBrowserIsCompatible()) {
-    map = new GMap2(document.getElementById("map_canvas"));
-    map.setCenter(new GLatLng(35.659683,139.742956), 16);
+    if (GBrowserIsCompatible()) {
+        map = new GMap2(document.getElementById("map_canvas"));
+        map.setCenter(new GLatLng(35.659683, 139.742956), 16);
 
-    var street = new GStreetviewOverlay();
-    map.addOverlay(street);
+        var street = new GStreetviewOverlay();
+        map.addOverlay(street);
 
-    var slatlng = new GLatLng(35.659683,139.742956);
-    var option = { latlng:slatlng };
+        var slatlng = new GLatLng(35.659683, 139.742956);
+        var option = {
+            latlng: slatlng
+        };
 
-    var scontainer = document.getElementById("street");
-    panorama = new GStreetviewPanorama(scontainer, option);
+        var scontainer = document.getElementById("street");
+        panorama = new GStreetviewPanorama(scontainer, option);
 
-    GEvent.addListener(map, "click", moveCenter);
-  }
+        GEvent.addListener(map, "click", moveCenter);
+    }
 }
 
-function moveCenter(overlay, latlng){
-  map.panTo(latlng);
-  panorama.setLocationAndPOV(latlng);
+function moveCenter(overlay, latlng) {
+    map.panTo(latlng);
+    panorama.setLocationAndPOV(latlng);
 }
 
 
-$(function(){
+$(function() {
     GUnload();
-    $("body").css("height",$(window).height() + 2 + "px")
-    $(window).scrollTop(1);    
+    $("body").css("height", $(window).height() + 2 + "px")
+    $(window).scrollTop(1);
 });
 
-$(window).on('load', function(event){
+$(window).on('load', function(event) {
     initialize();
 });
 
-$(window).on('scroll', function(event){
+$(window).on('scroll', function(event) {
     $(window).scrollTop(1);
     console.log('window');
 });
